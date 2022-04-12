c43e507e5595587aa339816083f74a7a9d463e5a
한시연 <97227087+hansIyEon@users.noreply.github.com>
2022-04-13 02:17:18 +0900

    Add files via upload

---
+++ MineSweeper.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <memory.h>
+#include <time.h>
+#include <ctype.h>
+
+#pragma warning(disable:4996)
+extern void playMineSweeper(int nBomb = 13);
