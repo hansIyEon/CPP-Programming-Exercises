0b77f2fb8a9878afbf403952da9d3f210ac84a39
한시연 <97227087+hansIyEon@users.noreply.github.com>
2022-05-27 18:37:23 +0900

    Add files via upload

---
+++ Week10_Homework1/Week10_Homework1/MonsterWorldGame.cpp
@@ -0,0 +1,18 @@
+#include "MonsterWorld.h"
+#include "VariousMonsters.h"
+#include <time.h>
+
+void main()
+{
+	srand((unsigned int)time(NULL));
+	int w = 16, h = 8;
+	MonsterWorld game(w, h);
+
+	game.add(new Zombie("좀비", "◎", rand() % w, rand() % h));
+	game.add(new Vampire("뱀파이어", "★", rand() % w, rand() % h));
+	game.add(new KGhost("고스트", "♥", rand() % w, rand() % h));
+	game.add(new Jiangshi("한시연", "↔", rand() % w, rand() % h, true));
+	game.add(new Jiangshi("한시욘", "↕", rand() % w, rand() % h, false));
+	game.play(500, 10);
+	printf("------게임 종료-------------------\n");
+}
